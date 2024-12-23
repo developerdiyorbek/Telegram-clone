d7a243ce0049d06c29a8bc218d2ba0717f0e2777
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-12-23 22:21:17 +0500

    profile section

--- client/lib/validation.ts
+++ client/lib/validation.ts
@@ -18,3 +18,24 @@ export const messageSchema = z.object({
   text: z.string().min(1, { message: "Message must be at least 1 character." }),
   image: z.string().optional(),
 });
+
+export const profileSchema = z.object({
+  firstName: z.string().min(2),
+  lastName: z.string().optional(),
+  bio: z.string().optional(),
+});
+
+export const oldEmailSchema = z
+  .object({
+    oldEmail: z
+      .string()
+      .email({ message: "Invalid email address, please check and try again." }),
+  })
+  .merge(emailSchema);
+
+export const confirmTextSchema = z
+  .object({ confirmText: z.string() })
+  .refine((data) => data.confirmText === "DELETE", {
+    message: "You must type DELETE to confirm.",
+    path: ["confirmText"],
+  });
